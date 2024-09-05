1c8f2b6a215e554fd90956935b7f0cdfdc706be1
1197chandu <jadhavc1197@gmail.com>
2024-09-05 16:02:55 +0530

    Created a initial folder structure and Genre Page routes

---
+++ src/components/GenrePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GenreButton from "./GenreButton";
+
+const GenrePage = () => {
+  return (
+    <div>
+      <GenreButton />
+    </div>
+  );
+};
+
+export default GenrePage;
